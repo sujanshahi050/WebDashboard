32c737fcf0fa9ac5e7fc21af8afa78bc4c8a1426
Sujan <sujanshahi050@outlook.com>
2021-01-21 15:11:36 -0600

    Added new indicators

---
+++ myapp/routes.py
@@ -0,0 +1,18 @@
+from myapp import app
+
+
+import json
+import plotly.utils
+from flask import render_template
+from scripts.data import return_figures
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+
+    figures = return_figures()
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+    print(figuresJSON)
+    return render_template('index.html', ids=ids, figuresJSON=figuresJSON)
